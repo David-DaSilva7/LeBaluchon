cd133967ade5517b83bf2d60c3034ea0ae84bcb7
DA SILVA <davidfctt@gmail.com>
2021-11-15 12:36:41 +0100

    Fonctionnement du bouton switch

--- LeBaluchon/LeBaluchon/Controller/TranslatorViewController.swift
+++ LeBaluchon/LeBaluchon/Controller/TranslatorViewController.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class TranslatorViewController: UIViewController {
-
+    
+    @IBOutlet weak var frTextfield: UITextField!
+    private var sourceLanguage = "fr"
+    private var targetLanguage = "en"
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    @IBAction func switchLanguage(_ sender: Any) {
+        switchLanguage()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.textFieldConfig()
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func textFieldConfig() {
+        frTextfield.textAlignment = .left
+        frTextfield.contentVerticalAlignment = .top
     }
-    */
-
+    
+    func switchLanguage(){
+        if sourceLanguage == "fr" {
+            sourceLanguage = "en"
+            self.firstLabel.text = "Anglais"
+            self.secondLabel.text = "Français"
+            
+        } else {
+            sourceLanguage = "fr"
+            self.firstLabel.text = "Français"
+            self.secondLabel.text = "Anglais"
+            
+        }
+    }
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
