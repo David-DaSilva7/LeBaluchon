076837e2f9e8cc8b3b4ec048155e989a75838425
DA SILVA <davidfctt@gmail.com>
2021-11-15 12:37:07 +0100

    integration key translate

--- LeBaluchon/LeBaluchon/Supporting files/APIKeys.swift
+++ LeBaluchon/LeBaluchon/Supporting files/APIKeys.swift
@@ -7,6 +7,7 @@
 
 struct APIKeys {
     static let fixerExchangeRate = "6c5f010d1d78d021aabd61e040e1e61e"
+    static let translate = "AIzaSyDwleJFcxyJbdNcAK2CCpI1j9O2CoWe5Dg"
     
     //    TODO: google translation
     //    TODO: open weather
