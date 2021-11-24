9f26e26c0226884dca332ceca8c16627b52f2f58
DA SILVA <davidfctt@gmail.com>
2021-11-24 17:08:16 +0100

    Debut du viewController weather

--- LeBaluchon/LeBaluchon/Controller/WeatherViewController.swift
+++ LeBaluchon/LeBaluchon/Controller/WeatherViewController.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    @IBOutlet weak var metzDescriptionLabel: UILabel!
+    @IBOutlet weak var metzTemperatureLabel: UILabel!
+    @IBOutlet weak var metzHumidityLabel: UILabel!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        callWeatherService(for: .paris)
+//
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.callWeatherService(for: .metz)
     }
     
-
-
-
+    
+    private func callWeatherService(for city: Cities) {
+        WeatherService.shared.getWeather(for: city) { [weak self] success, weather in
+            guard let self = self else { return }
+            
+            if success, let weather = weather {
+                if city == .metz {
+//                    self.metzDescriptionLabel.text = "\(weather.weather.description)"
+                    self.metzTemperatureLabel.text = "\(Int(weather.main.temp))°C"
+                    self.metzHumidityLabel.text = "\(weather.main.humidity)%"
+                
+                } else {
+                    self.presentAlert(title: "Petit problème",
+                                      message: "OpenWeather n'a pas répondu.\nVeuillez réessayer.")
+                }
+            }
+        }
+    }
 }
