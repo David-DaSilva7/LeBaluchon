68ca718cdbaf24b417a2d5536e1e38e0214b7a0c
DA SILVA <davidfctt@gmail.com>
2021-11-26 11:24:14 +0100

    integration func capitalize

---
+++ LeBaluchon/LeBaluchon/Extension/Capitalize.swift
@@ -0,0 +1,19 @@
+//
+//  Capitalize.swift
+//  LeBaluchon
+//
+//  Created by David Da Silva on 25/11/2021.
+//
+
+import Foundation
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
+  
