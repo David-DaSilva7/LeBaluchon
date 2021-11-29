1e9e69250a427ee9665c472062bce644d554736c
DA SILVA <davidfctt@gmail.com>
2021-11-29 16:09:16 +0100

    changement de Clé

--- LeBaluchon/LeBaluchon/Supporting files/APIKeys.swift
+++ LeBaluchon/LeBaluchon/Supporting files/APIKeys.swift
@@ -8,6 +8,6 @@
 struct APIKeys {
     static let fixerExchangeRate = "6c5f010d1d78d021aabd61e040e1e61e"
     static let GoogleTranslation = "AIzaSyDwleJFcxyJbdNcAK2CCpI1j9O2CoWe5Dg"
-    static let OpenWeatherMap = "5604996f5562383aa6b487e81da78d63"
+    static let OpenWeatherMap = "dbb5883cf4d0f323df45f87aa00da6ca"
 }
 
